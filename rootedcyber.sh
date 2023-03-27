349ae886580ba23009f7aa0b8ac18dbc92dc6180
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-03-28 05:15:10 +0530

    Update rootedcyber.sh

--- rootedcyber.sh
+++ rootedcyber.sh
@@ -3,6 +3,7 @@ cd Ultroid
 pip install --upgrade pip
 pip3 install -r req*
 pip3 install -r res*/st*/op*
-pip3 install pytgcalls
+git clone https://github.com/rooted-cyber/install-package
+pip3 install inst*pa*e/pytg*
 pip3 -V
 bash startup
