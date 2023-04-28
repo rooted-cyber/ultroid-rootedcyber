65e9008b956f579b2473234113b7186fd42a4c09
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-04-28 09:06:12 +0530

    Update rootedcyber.sh

--- rootedcyber.sh
+++ rootedcyber.sh
@@ -1,1 +1,1 @@
-sh -c "$(curl -fsSl https://gist.githubusercontent.com/rooted-cyber/52ca927a2f491e0038ebe07f61749988/raw/3a60f81262dd3bc2ab0eec799a1e74e3b7adc2da/latest)"
+sh -c "$(curl -fsSl https://gist.githubusercontent.com/rooted-cyber/d81ab2b6c22ad40497f8d08133c7e8f3/raw/ult.sh)"
