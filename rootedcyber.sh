f9c4df6619048b2325882481e5c1004b3e89de3e
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-06-24 21:04:25 +0530

    Update rootedcyber.sh

--- rootedcyber.sh
+++ rootedcyber.sh
@@ -1,5 +1,5 @@
-wget https://gist.githubusercontent.com/rooted-cyber/143d3602a7b7dc476a9e6a9c4bf59a02/raw/19b5df02adc2b08c25bd10949cea0ad9c6ffeb64/deploy
-bash depl*
+#wget https://gist.githubusercontent.com/rooted-cyber/143d3602a7b7dc476a9e6a9c4bf59a02/raw/19b5df02adc2b08c25bd10949cea0ad9c6ffeb64/deploy
+#bash depl*
 git clone https://github.com/TeamUltroid/Ultroid/
 cd Ultroid
 pip install --upgrade pip
@@ -7,5 +7,6 @@ pip3 install -r req*
 pip3 install -r res*/st*/op*
 git clone https://github.com/rooted-cyber/install-package
 pip3 install inst*pa*e/pytg*
+co ins*pac*/plu/*py plugin
 pip3 -V
 bash startup
