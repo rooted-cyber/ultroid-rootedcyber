85fc1c52693a53d413e3ea9c5e84f64fa8d913fc
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-03-21 14:00:27 +0530

    Update rootedcyber.sh

--- rootedcyber.sh
+++ rootedcyber.sh
@@ -1,8 +1,10 @@
 #wget https://gist.githubusercontent.com/rooted-cyber/143d3602a7b7dc476a9e6a9c4bf59a02/raw/19b5df02adc2b08c25bd10949cea0ad9c6ffeb64/deploy
 #bash depl*
 a() {
-git clone 
-cd 
+git clone https://github.com/rooted-cyber/Ultroid-new
+cd Ultr*new
+unsip root*
+cd rooted
 pip install --upgrade pip
 pip3 install -r req*
 pip3 install -r res*/st*/op*
